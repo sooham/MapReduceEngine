2357c40483cbca1016827a92590aa530e56f3d55
c4osorio <c4osorio@f7ef972d-84f5-410e-9d84-66ef585507c6>
2016-03-13 01:01:15 +0000

    Created mapreduce.c

---
+++ a3/mapreduce.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+
+/**
+ * Reads the filename of all files in a directory,
+ * and sends them evenly to map worker pipes.
+ * @param path the path to the directory.
+ */
+void walk_directory(char *path){
+	
+}
+
+
+int main(){
+
+}
