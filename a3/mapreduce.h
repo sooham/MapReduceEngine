de049fcfe23c1b77d9ce8a165a99cfe959d93356
c4rafizs <c4rafizs@f7ef972d-84f5-410e-9d84-66ef585507c6>
2016-03-21 02:22:07 +0000

    dynamic memory allocation for path

--- a3/mapreduce.h
+++ a3/mapreduce.h
@@ -39,7 +39,7 @@ typedef struct keyValues {
 typedef struct mapReduceLogistics {
     int nmapworkers;
     int nreduceworkers;
-    char dirname[MAX_FILENAME];
+    char *dirname;
 } MapReduceLogistics;
 
 /*
